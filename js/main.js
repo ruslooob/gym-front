50847345b6133f9d8b4adf6957ede32f05f22fca
ruslooob <rm952576@gmail.com>
2021-06-24 23:11:57 +0300

    added create client form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let tbody = document.querySelector('tbody');
 
-
 async function fetchData(url) {
 	return (await fetch(url)).json();
 }
@@ -10,16 +9,7 @@ async function main() {
 	// массив объектов
 	let users = await fetchData(getAllUsersURL);
 	for (user of users) {
-		let tr = document.createElement('tr');
-		let td1 = document.createElement('td');
-		let td2 = document.createElement('td');
-		let td3 = document.createElement('td');
-		td1.textContent = user.id;
-		td2.textContent = user.fullName;
-		td3.textContent = user.telNumber;
-		tr.appendChild(td1);
-		tr.appendChild(td2);
-		tr.appendChild(td3);
+		let tr = createTr();
 		tbody.appendChild(tr);
 	}
 }
@@ -27,4 +17,16 @@ async function main() {
 main();
 
 
-
+function createTr() {
+	let tr = document.createElement('tr');
+	let td1 = document.createElement('td');
+	let td2 = document.createElement('td');
+	let td3 = document.createElement('td');
+	td1.textContent = user.id;
+	td2.textContent = user.fullName;
+	td3.textContent = user.telNumber;
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tr.appendChild(td3);
+	return tr;
+}
